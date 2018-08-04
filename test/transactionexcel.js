e3874bb6aaa21df4eba5e4106f529c6549aff057
eightandseven <reinyear@gmail.com>
2018-08-04 20:11:45 +0800

    working excel

--- test/transactionexcel.js
+++ test/transactionexcel.js
@@ -175,25 +175,19 @@ let apptransact = [
         total: 244
     }
 ]
-const filename = 'C:\\Users\\rein\\Desktop\\test.xlsx'
+const filename = 'C:\\Users\\Reinier\\Desktop\\test.xlsx'
 
 const xl = require('excel4node')
 const fs = require('fs')
 
-function saveAsExcel(columns, transaction, filename) {
+function saveAsExcel(columns, transaction, filename, callback) {
     const wb = new xl.Workbook()
 
+    // worksheets
     const ws = wb.addWorksheet('Transactions')
     const ws2 = wb.addWorksheet('Summary Report')
+    const ws3 = wb.addWorksheet('Columns')
 
-    // const numberstyle = wb.createStyle({
-    //     font : {
-    //         color : '#000000',
-    //         size : 12
-    //     },
-    //     numberFormat : 'Php ##.00;'
-    // })
-    
     // Transaction Worksheet
     // columns cell
     columnname = []
@@ -217,8 +211,9 @@ function saveAsExcel(columns, transaction, filename) {
     let xlrowcounter = 1
 
     // summary report object
-    let summaryreport = {}
+    let summaryreport = []
 
+    // transactions worksheet
     // print date
     ws.cell(xlrowcounter, xlcolumncounter).string('date_time')
     xlcolumnobj['date_time'] = codeletternumber + xlcolumncounter
@@ -245,7 +240,7 @@ function saveAsExcel(columns, transaction, filename) {
     xlrowcounter += 1
 
     // print rows
-    transaction.forEach(item => {
+    transaction.forEach((item, index) => {
         // print date
         ws.cell(xlrowcounter, xlcolumnobj['date_time'] - codeletternumber).string(item.date)
         
@@ -261,7 +256,25 @@ function saveAsExcel(columns, transaction, filename) {
                     ? String.fromCharCode(xlcolumnobj[i['name']]) + xlrowcounter.toString()
                     : '+' + String.fromCharCode(xlcolumnobj[i['name']]) + xlrowcounter.toString()
 
-                
+                if (index == 0) {
+                    summaryreport.push({
+                        name : i['name'],
+                        price : i['value']['price'],
+                        quantity : i['value']['quantity']
+                    })
+                } else {
+                    summaryreport.forEach(r => {
+                        if (r['name'] === i['name'] && r['price'] == i['value']['price']) {
+                            r['quantity'] += i['value']['quantity']
+                        } else if (r['name'] === i['name'] && r['price'] != i['value']['price']) {
+                            summaryreport.push({
+                                name : i['name'],
+                                price : i['value']['price'],
+                                quantity : i['value']['quantity']
+                            })    
+                        }
+                    })
+                }
             }
         })
         // print total
@@ -271,13 +284,82 @@ function saveAsExcel(columns, transaction, filename) {
         xlrowcounter += 1
     })
 
-    // ws.cell(1, 1).number(100).style(style)
-    // ws.cell(1, 2).number(200).style(style)
-    // ws.cell(1, 3).formula('A1 + B1').style(style)
-    // ws.cell(2, 1).string('string').style(style)
-    // ws.cell(3, 1).bool(true).style(style).style({font: {size:20}})
+    // summary report sheet
+    xlrowcounter = 1
+    xlcolumncounter = 1
+    const summarycolumn = ['prod_name', 'prod_price', 'prod_sold', 'prod_sale']
+    // print columns
+    summarycolumn.forEach(item => {
+        ws2.cell(xlrowcounter, xlcolumncounter).string(item)
+        xlcolumncounter += 1
+    })
+
+    // add row and reset column
+    xlrowcounter += 1
+    xlcolumncounter = 1
+    
+    // print product
+    let totalformula = ''
+    summaryreport.forEach(item => {
+        // print name
+        ws2.cell(xlrowcounter, xlcolumncounter).string(item['name'])
+        xlcolumncounter += 1
+        // print price
+        ws2.cell(xlrowcounter, xlcolumncounter).number(item['price'])
+        xlcolumncounter += 1
+        // print quantity
+        ws2.cell(xlrowcounter, xlcolumncounter).number(item['quantity'])
+        xlcolumncounter += 1
+        // print sale
+        const formula = String.fromCharCode( codeletternumber + summarycolumn.indexOf('prod_price') + 1 ) + xlrowcounter.toString() + '*' +
+                        String.fromCharCode( codeletternumber + summarycolumn.indexOf('prod_sold') + 1) + xlrowcounter.toString()
+        ws2.cell(xlrowcounter, xlcolumncounter).formula(formula)
+
+        totalformula += totalformula === '' 
+            ? String.fromCharCode(xlcolumncounter + codeletternumber) + xlrowcounter.toString()
+            : '+' + String.fromCharCode(xlcolumncounter + codeletternumber) + xlrowcounter.toString()
+        // add row and reset column
+        xlrowcounter += 1
+        xlcolumncounter = 1
+    })
+    // print total
+    ws2.cell(xlrowcounter, summarycolumn.indexOf('prod_sold') + 1).string('total')
+    ws2.cell(xlrowcounter, summarycolumn.indexOf('prod_sale') + 1).formula(totalformula)
+
+    // column worksheet
+    xlcolumncounter = 1
+    xlrowcounter = 1
+    let appcolumns = ['column_id', 'column_name', 'column_require', 'column_desc', 'column_worn', 'column_price']
+    
+    // print column
+    appcolumns.forEach(item => {
+        ws3.cell(xlrowcounter, xlcolumncounter).string(item)
+        xlcolumncounter += 1
+    })
+
+    // add row and reset column
+    xlrowcounter += 1
+
+    // print application columns
+    columns.forEach(item => {
+        appcolumns.forEach(i => {
+            if (typeof item[i] !== 'undefined') {
+                if (typeof item[i] === 'string') {
+                    ws3.cell(xlrowcounter, appcolumns.indexOf(i) + 1).string(item[i])
+                } else {
+                    ws3.cell(xlrowcounter, appcolumns.indexOf(i) + 1).number(item[i])
+                }
+                
+            }
+        })
+        xlrowcounter += 1
+    })
+
     wb.write(filename)
+    callback()
 }
 
 if (fs.existsSync(filename)) fs.unlinkSync(filename)
-saveAsExcel(appsettings.columns, apptransact, filename)
+saveAsExcel(appsettings.columns, apptransact, filename, () => {
+
+})
